dff324e33e4fc2f0b1a6903ecd754b74650089f1
Home\Daniel <dankenji27@gmail.com>
2015-08-23 20:14:33 -0300

    13 done
    One more done

---
+++ exercise13.py
@@ -0,0 +1,18 @@
+﻿# Fibonacci sequence. Asks user for how many numbers. 
+
+def fib(num):
+    fibnum = []
+    a, b = 0, 1
+    count = 0
+    while count < num:
+        fibnum.append(b)
+        a, b = b, a + b
+        count += 1
+    return fibnum
+
+
+if __name__ == "__main__":
+    print("Generate how many numbers? Input must be integer. ")
+    num = int(input())
+    print(fib(num))
+    
